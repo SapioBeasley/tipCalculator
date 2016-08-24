c62a665973a63a1d976603cc7ea9096347f73a8e
Andreas Beasley <andreas.beasley@gmail.com>
2016-08-23 20:42:54 -0700

    initial top calculator

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billInput: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var percentages: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTapCloseKeyboard(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.1,0.15,0.2]
+        let bill = Double(billInput.text!) ?? 0
+        let tip = bill * tipPercentages[percentages
+            .selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
 }
 
